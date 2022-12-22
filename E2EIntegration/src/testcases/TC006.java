c203d955f9dfd3a7a32458812570f9d9bd7c17de
Priyabaghele <101499610+Priyabaghele@users.noreply.github.com>
2022-12-22 17:42:27 +0530

    tc006 is added

---
+++ E2EIntegration/src/testcases/TC006.java
@@ -0,0 +1,21 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC006 {
+	
+	@Test
+	public void testcase1() {
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElementById("email");
+		driver.findElement(By.id("pass"));
+		driver.quit();
+	}
+
+}
+	
+
